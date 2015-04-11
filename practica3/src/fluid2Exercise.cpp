23063d84d597430725933a4fa4e2f742cf3af286
julioUrjc <j.martinsae@alumnos.urjc.es>
2015-04-11 18:26:11 +0200

    Advection Method

--- practica3/src/fluid2Exercise.cpp
+++ practica3/src/fluid2Exercise.cpp
@@ -2,31 +2,100 @@
 #include "scene.h"
 #include "pcg_solver.h"
 
-namespace
-{
+namespace{
 	//////////////////////////////////////////////
 	// Add any custom classes or functions here! //
 	//////////////////////////////////////////////
-    
+	
+	inline int clamp(int a, int b, int c){	return a<b ? b:(a>c ? c:a); }
+
+	float interpolation(Vec2 idPos, Array2<float>& inkAux){
+		// Calculate min and max position
+		Vec2 posMin(floorf(idPos.x), floorf(idPos.y));
+		Vec2 posMax(ceilf(idPos.x), ceilf(idPos.y));
+		Vec2 mid(idPos-posMin);
+
+		Index2 min((int)posMin.x, (int)posMin.y);
+		Index2 max((int)posMax.x, (int)posMax.y);
+		// Calculate index cells
+		Index2 s= inkAux.getSize();
+		Index2 id1(clamp(min.x, 0, s.x-1), clamp(min.y, 0, s.y-1));
+		Index2 id2(clamp(max.x, 0, s.x-1), clamp(min.y, 0, s.y-1));
+		Index2 id3(clamp(min.x, 0, s.x-1), clamp(max.y, 0, s.y-1));
+		Index2 id4(clamp(max.x, 0, s.x-1), clamp(max.y, 0, s.y-1));
+
+		// Bilinear interpolation
+		// alfa= x-i	beta= y-j
+		// U_alfa_j=  (1-alfa)*u_ij + alfa*u_i + 1j
+		// U_alfa_j+1= (1-alfa)*u_ij + 1 + alfa*u_i + 1j + 1
+		// I= (1-beta)u_alfa_j + beta*u_alfa_j + 1
+		return (inkAux[id1]*(1.0f-mid.x)+inkAux[id2]*mid.x)* (1.0f-mid.y) + (inkAux[id3]*(1.0f-mid.x)+inkAux[id4]* mid.x)* mid.y;
+	}	
 }
 
-// advection
-void Fluid2::fluidAdvection( const float dt )
-{
+/***** Advection *****/
+void Fluid2::fluidAdvection( const float dt ){
     // ink advection
-    {
-    }
+		Array2<float> inkAux(ink);																// Copy for the interpolation
+		Index2 size = ink.getSize();															// Ink size
+
+		for(int i= 0; i<size.x; ++i){
+			for(int j= 0; j<size.y; ++j){
+				Index2 id(i, j);																// Global index
+
+				Vec2 position(grid.getCellPos(id));												// Global coordinates
+				Vec2 velocity((velocityX[id] + velocityX[Index2(i+1, j)])*0.5f, (velocityY[id] + velocityY[Index2(i, j+1)])*0.5f); // Advencion Method
+				Vec2 newPosition(position - dt*velocity);										// Integrate position
+
+				ink[id] = interpolation(grid.getCellIndex(newPosition), inkAux);				// Bilinear interpolation
+			}
+		}
 
     // velocity advection
-    {
-    }
+			Array2<float> xCopy(velocityX);					 									// Copy X for the interpolation
+			Array2<float> yCopy(velocityY);					 									// Copy Y for the interpolation
+			
+			Index2 sizeX = velocityX.getSize();				 									// X size
+			Index2 sizeY = velocityY.getSize();				 									// Y size
+
+			for(int i= 0; i<sizeX.x; ++i){
+				for(int j= 0; j<sizeX.y; ++j){
+					Index2 id(i, j);
+					// Calculate index of the previus velocities in X to compute the velocityX
+					Index2 idX1(clamp(i-1, 0, sizeY.x-1), clamp(j  , 0, sizeY.y-1));
+					Index2 idX2(clamp(i  , 0, sizeY.x-1), clamp(j  , 0, sizeY.y-1));
+					Index2 idX3(clamp(i-1, 0, sizeY.x-1), clamp(j+1, 0, sizeY.y-1));
+					Index2 idX4(clamp(i  , 0, sizeY.x-1), clamp(j+1, 0, sizeY.y-1));
+
+					Vec2 position(grid.getFaceXPos(id));												 // Calculate global coordinates VelocityX
+					Vec2 velocity(xCopy[id], (yCopy[idX1]+yCopy[idX2]+yCopy[idX3]+yCopy[idX4])*0.25f);   // Advencion Method
+					Vec2 newXPosition(position - dt*velocity);     										 // Integrate X position
+
+					velocityX[id] = interpolation(grid.getFaceIndex(newXPosition, 0), xCopy);			 // Bilinear interpolation
+				}
+			}
+
+			for(int i= 0; i<sizeY.x; ++i){
+				for(int j= 0; j<sizeY.y; ++j){
+					Index2 id(i, j);
+					// Calculate index of the previus velocities in Y to compute the velocityY
+					Index2 idY1(clamp(i  , 0, sizeX.x-1), clamp(j-1, 0, sizeX.y-1));
+					Index2 idY2(clamp(i  , 0, sizeX.x-1), clamp(j  , 0, sizeX.y-1));
+					Index2 idY3(clamp(i+1, 0, sizeX.x-1), clamp(j-1, 0, sizeX.y-1));
+					Index2 idY4(clamp(i+1, 0, sizeX.x-1), clamp(j  , 0, sizeX.y-1));
+
+					Vec2 position(grid.getFaceYPos(id));											   // Calculate global coordinates VelocityY
+					Vec2 velocity((xCopy[idY1]+xCopy[idY2]+xCopy[idY3]+xCopy[idY4])*0.25f, yCopy[id]); // Advencion Method
+					Vec2 newYPosition(position-dt*velocity);     									   // Integrate Y position
+
+					velocityY[id] = interpolation(grid.getFaceIndex(newYPosition, 1), yCopy);		   // Bilinear interpolation
+				}
+			}
 }
 
-// emission
-void Fluid2::fluidEmission()
-{
-	if( Scene::testcase >= Scene::SMOKE )
-	{
+/***** Emission *****/
+void Fluid2::fluidEmission(){
+	if( Scene::testcase >= Scene::SMOKE ){
         // emit source ink
         {
         }
@@ -37,28 +106,22 @@ void Fluid2::fluidEmission()
 }
 
 // volume forces
-void Fluid2::fluidVolumeForces( const float dt )
-{
-	if( Scene::testcase >= Scene::SMOKE )
-	{
+void Fluid2::fluidVolumeForces( const float dt ){
+	if( Scene::testcase >= Scene::SMOKE ){
         // gravity
 	}
 }
 
 // viscosity
-void Fluid2::fluidViscosity( const float dt )
-{
-	if( Scene::testcase >= Scene::SMOKE )
-	{
+void Fluid2::fluidViscosity( const float dt ){
+	if( Scene::testcase >= Scene::SMOKE ){
         // viscosity
 	}
 }
 
 // pressure
-void Fluid2::fluidPressureProjection( const float dt )
-{
-	if( Scene::testcase >= Scene::SMOKE )
-	{
+void Fluid2::fluidPressureProjection( const float dt ){
+	if( Scene::testcase >= Scene::SMOKE ){
         // pressure
 	}
 }
---
+++ practica3/src/fluid2ExerciseRaquel.cpp
@@ -0,0 +1,149 @@
+
+#include "scene.h"
+#include "pcg_solver.h"
+
+namespace
+{
+	//////////////////////////////////////////////
+	// Add any custom classes or functions here! //
+	//////////////////////////////////////////////
+
+	inline int clamp(int x, int a, int b){
+		return (x<a) ? a : ((x>b) ? b : x);
+	}
+
+	inline float bilerp(Vec2 pos, Array2<float> field, Index2 size){
+		const Vec2 posMin(floorf(pos.x), floorf(pos.y));
+		const Vec2 posMax(ceilf(pos.x), ceilf(pos.y));
+		const Vec2 t(pos - posMin);
+
+		const Index2 id1(clamp((int)posMin.x, 0, size.x - 1), clamp((int)posMin.y, 0, size.y - 1));
+		const Index2 id2(clamp((int)posMax.x, 0, size.x - 1), clamp((int)posMin.y, 0, size.y - 1));
+		const Index2 id3(clamp((int)posMin.x, 0, size.x - 1), clamp((int)posMax.y, 0, size.y - 1));
+		const Index2 id4(clamp((int)posMax.x, 0, size.x - 1), clamp((int)posMax.y, 0, size.y - 1));
+
+		///// Bilineal interpolation - General case
+		/// alfa = x - i
+		/// beta = y - j
+		/// U_alfa_j =  (1 - alfa) * u_ij + alfa * u_i+1j
+		/// U_alfa_j+1 = (1 - alfa) * u_ij+1 + alfa * u_i+1j+1
+		/// I = (1 - beta)u_alfa_j + beta * u_alfa_j+1
+
+		const float y1 = (1.0f - t.x) * field[id1] + t.x * field[id2];
+		const float y2 = (1.0f - t.x) * field[id3] + t.x * field[id4];
+		const float value = (1 - t.y) * y1 + t.y * y2;
+		return value;
+
+	}
+
+    
+}
+
+// advection
+void Fluid2::fluidAdvection( const float dt )
+{
+    // ink advection
+    {
+		Array2<float> inkAux(ink);
+		for (unsigned int i = 0; i < ink.getSize().x; ++i){
+			for (unsigned int j = 0; j < ink.getSize().y; ++j){
+				const Index2 id(i, j);
+
+				/// Calculate global coordinates
+				const Vec2 pos(grid.getCellPos(id));
+				/// Calculate index of the velocitis
+				const Index2 idvXaux(clamp(i + 1, 0, velocityX.getSize().x - 1), clamp(j, 0, velocityX.getSize().y - 1));
+				const Index2 idvYaux(clamp(i, 0, velocityY.getSize().x - 1), clamp(j+1, 0, velocityY.getSize().y - 1));
+				/// Calculate advencion method
+				const Vec2 vel((velocityX[id] + velocityX[idvXaux]) * 0.5f, (velocityY[id] + velocityY[idvYaux]) * 0.5f);
+				const Vec2 endpos(pos - dt * vel);
+				/// Bilineal interpolation with index
+				ink[id] = bilerp(grid.getCellIndex(endpos),inkAux,inkAux.getSize());;
+			}
+		}
+    }
+	
+    // velocity advection
+    {
+		Array2< float > vXaux(velocityX);
+		Array2< float > vYaux(velocityY);
+		const Index2 sizeVx = velocityX.getSize();
+		const Index2 sizeVy = velocityY.getSize();
+		for (unsigned int i = 0; i < sizeVx.x; ++i){
+			for (unsigned int j = 0; j < sizeVx.y; ++j){
+				const Index2 id(i, j);
+				/// Calculate global coordinates VelocityX
+				const Vec2 pos(grid.getFaceXPos(id));
+				/// Calculate index of the previus velocities in X to compute the velocityX
+				const Index2 id1(clamp(i-1, 0, sizeVy.x-1), clamp(j,   0, sizeVy.y-1));
+				const Index2 id2(clamp(i,   0, sizeVy.x-1), clamp(j,   0, sizeVy.y-1));
+				const Index2 id3(clamp(i-1, 0, sizeVy.x-1), clamp(j+1, 0, sizeVy.y-1));
+				const Index2 id4(clamp(i,   0, sizeVy.x-1), clamp(j+1, 0, sizeVy.y-1));
+				/// Compute advection methond
+				const Vec2 vel(vXaux[id], (vYaux[id1] + vYaux[id2] + vYaux[id3] + vYaux[id4])*0.25f);
+				const Vec2 endpos(pos - dt*vel);
+				/// Bilineal interpolation with index, face index in axis 0, horizontal
+				velocityX[id] = bilerp(grid.getFaceIndex(pos, 0),vXaux,vXaux.getSize());
+			}
+		}
+		for (unsigned int i = 0; i < sizeVy.x; ++i){
+			for (unsigned int j = 0; j < sizeVy.y; ++j){
+				const Index2 id(i, j);
+				/// Calculate global coordinates VelocityY
+				const Vec2 pos(grid.getFaceYPos(id));
+				/// Calculate index of the previus velocities in X to compute the velocityX
+				const Index2 id1(clamp(i,   0, sizeVx.x - 1), clamp(j-1, 0, sizeVx.y - 1));
+				const Index2 id2(clamp(i,   0, sizeVx.x - 1), clamp(j-1, 0, sizeVx.y - 1));
+				const Index2 id3(clamp(i+1, 0, sizeVx.x - 1), clamp(j,   0, sizeVx.y - 1));
+				const Index2 id4(clamp(i+1, 0, sizeVx.x - 1), clamp(j,   0, sizeVx.y - 1));
+				/// Compute advection methond
+				const Vec2 vel(vXaux[id], (vYaux[id1] + vYaux[id2] + vYaux[id3] + vYaux[id4])*0.25f);
+				const Vec2 endpos(pos - dt*vel);
+				/// Bilineal interpolation with index, face index in axis 0, horizontal
+				velocityY[id] = bilerp(grid.getFaceIndex(pos, 1), vXaux, vXaux.getSize());
+			}
+		}
+    }
+	
+}
+
+// emission
+void Fluid2::fluidEmission()
+{
+	if( Scene::testcase >= Scene::SMOKE )
+	{
+        // emit source ink
+        {
+        }
+        // emit source velocity
+        {
+        }
+	}
+}
+
+// volume forces
+void Fluid2::fluidVolumeForces( const float dt )
+{
+	if( Scene::testcase >= Scene::SMOKE )
+	{
+        // gravity
+	}
+}
+
+// viscosity
+void Fluid2::fluidViscosity( const float dt )
+{
+	if( Scene::testcase >= Scene::SMOKE )
+	{
+        // viscosity
+	}
+}
+
+// pressure
+void Fluid2::fluidPressureProjection( const float dt )
+{
+	if( Scene::testcase >= Scene::SMOKE )
+	{
+        // pressure
+	}
+}
